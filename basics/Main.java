3d2536a9ed310b7164a6fc4d8e429b728b0541af
ceylinbrooks <ceylinbrooks@gmail.com>
2021-03-23 16:47:01 -0500

    Animals Commit

--- basics/Main.java
+++ basics/Main.java
@@ -1,16 +1,16 @@
-public class pluralize {
+import java.util.Random;
+
+public class Main {
   public static void main (String[] args) {
-    int pluralWord = ;
-    System.out.println();
+  int dogCount = 1;
+System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+int catCount = 2;
+System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+int turtleCount = 0;
+System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + "."); 
 
   }
 }
 
-// int dogCount = 1;
-// System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
-
-// int catCount = 2;
-// System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
-
-// int turtleCount = 0;
-// System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
