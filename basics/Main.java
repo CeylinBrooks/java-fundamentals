399c4bebe6075a35bbe71ff36474f30c5c54b4ce
ceylinbrooks <ceylinbrooks@gmail.com>
2021-03-23 16:41:37 -0500

    working on lab01/part01

---
+++ basics/Main.java
@@ -0,0 +1,16 @@
+public class pluralize {
+  public static void main (String[] args) {
+    int pluralWord = ;
+    System.out.println();
+
+  }
+}
+
+// int dogCount = 1;
+// System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+// int catCount = 2;
+// System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+// int turtleCount = 0;
+// System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
